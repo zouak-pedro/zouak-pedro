8f53c83286a03b5b97ebd06649e6441983ad2c69
zouak-pedro <zouakpedro@gmail.com>
2026-09-13 22:14:42 +0100

    chore: keep only README.md for GitHub profile

--- core/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoreConfig(AppConfig):
-    name = 'core'
-
-    def ready(self):
-        from . import signals
-        signals.connect_model_audit_signals()
--- core/querysets.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class ClinicQuerySet(models.QuerySet):
-
-    def for_clinic(self, clinic):
-        return self.filter(clinic=clinic)
-
